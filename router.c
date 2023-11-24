19c8b025f431a3910ddc5c09cac5e3327e656e33
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-11-24 18:16:30 +0200

    Update router.c

---
+++ router.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*router -  checks input string for format type
+*@fmt: character
+*Return: 0 on success
+*/
+int (*router(char fmt))(va_list, char *, int)
+{
+int i;
+routes router[] = {
+{'s', buffer_string},
+{'c', buffer_char},
+{'i', buffer_int},
+{'d', buffer_int},
+{'b', buffer_int_b},
+{'\0', NULL}
+};
+
+i = 0;
+
+while (router[i].c != '\0')
+{
+if (router[i].c == fmt)
+return (router[i].f);
+
+i++;
+}
+
+return (0);
+}
