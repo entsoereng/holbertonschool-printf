ee6e249c0b9916c3182300178ea0d51a23e57d2e
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-25 16:08:15 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,50 +5,50 @@
 #include "main.h"
 
 /**
-* _printf - prints anything
-* @format: format string
-*
-* Return: number of characters printed (excluding null byte)
-*/
+ * _printf - prints anything
+ * @format: format string
+ *
+ * Return: number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-int i;
-int index = 0;
-int (*routing)(va_list, char *, int);
+    if (format == NULL)
+        return -1;
 
-char buffer[1024];
-va_list ap;
+    int i;
+    int index = 0;
+    int (*routing)(va_list, char *, int);
 
-if (format == NULL)
-return -1;
+    char buffer[1024];
+    va_list ap;
 
-va_start(ap, format);
+    va_start(ap, format);
 
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[index] = format[i];
-index++;
-}
-else if (format[i + 1] != '\0')
-{
-routing = router(format[i + 1]);
-if (routing != NULL)
-{
-index = routing(ap, buffer, index);
-i++;
-}
-else
-{
-buffer[index] = '%';
-index++;
-}
-}
-}
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            buffer[index] = format[i];
+            index++;
+        }
+        else if (format[i + 1] != '\0')
+        {
+            routing = router(format[i + 1]);
+            if (routing != NULL)
+            {
+                index = routing(ap, buffer, index);
+                i++;
+            }
+            else
+            {
+                buffer[index] = '%';
+                index++;
+            }
+        }
+    }
 
-va_end(ap);
+    va_end(ap);
 
-write(1, buffer, index);
-return index;
+    write(1, buffer, index);
+    return index;
 }
