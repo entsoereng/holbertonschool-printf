906b0bd239864a77ba49c02a21c7664834373107
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-25 16:07:18 +0200

    Update formatters.c

--- formatters.c
+++ formatters.c
@@ -2,94 +2,88 @@
 #include "main.h"
 
 /**
-* buffer_string - prints string
-* @ap: va_list
-* @buffer: character string
-* @index: position in string
-* Return: integer
-*/
+ * buffer_string - prints string
+ * @ap: va_list
+ * @buffer: character string
+ * @index: position in string
+ * Return: integer
+ */
 int buffer_string(va_list ap, char *buffer, int index)
 {
-char *s;
+    char *s = va_arg(ap, char *);
 
-s = va_arg(ap, char *);
+    if (s == NULL)
+    {
+        // Handle NULL string
+        index = _strcpy(buffer, "(null)", index);
+    }
+    else
+    {
+        index = _strcpy(buffer, s, index);
+    }
 
-index = _strcpy(buffer, s, index);
-
-return (index);
+    return index;
 }
 
 /**
-* buffer_char - prints char
-* @ap: va_list
-* @buffer: character string
-* @index: position in string
-* Return: integer
-*/
+ * buffer_char - prints char
+ * @ap: va_list
+ * @buffer: character string
+ * @index: position in string
+ * Return: integer
+ */
 int buffer_char(va_list ap, char *buffer, int index)
 {
-char s;
-
-s = va_arg(ap, int);
-
-buffer[index] = s;
-
-return (index + 1);
+    char s = va_arg(ap, int);
+    buffer[index] = s;
+    return index + 1;
 }
 
 /**
-* buffer_int - prints integer
-* @ap: va_list
-* @buffer: character string
-* @index: position in string
-* Return: integer
-*/
+ * buffer_int - prints integer
+ * @ap: va_list
+ * @buffer: character string
+ * @index: position in string
+ * Return: integer
+ */
 int buffer_int(va_list ap, char *buffer, int index)
 {
-char *s;
-int in;
+    int in = va_arg(ap, int);
 
-in = va_arg(ap, int);
+    if (in < 0)
+    {
+        index = buffer_check(buffer, index);
+        index = negative_copy(buffer, index);
+        in = _abs(in);
+    }
 
-if (in < 0)
-{
-index = buffer_check(buffer, index);
-index = negative_copy(buffer, index);
-in = _abs(in);
-}
+    char *s = int_string(in);
+    index = _strcpy(buffer, s, index);
 
-s = int_string(in);
-
-index = _strcpy(buffer, s, index);
-return (index);
+    return index;
 }
 
 /**
-* buffer_int_b - prints binary integer
-* @ap: va_list
-* @buffer: character string
-* @index: position in string
-* Return: integer
-*/
+ * buffer_int_b - prints binary integer
+ * @ap: va_list
+ * @buffer: character string
+ * @index: position in string
+ * Return: integer
+ */
 int buffer_int_b(va_list ap, char *buffer, int index)
 {
-char *s;
-int in;
+    int in = va_arg(ap, int);
 
-in = va_arg(ap, int);
+    if (in < 0)
+    {
+        index = buffer_check(buffer, index);
+        in = _abs(in);
+        buffer[index] = '-';
+        index++;
+    }
 
-if (in < 0)
-{
-index = buffer_check(buffer, index);
+    char *s = int_b_string(in);
+    index = _strcpy(buffer, s, index);
 
-in = _abs(in);
-buffer[index] = '-';
-index++;
-}
-
-s = int_b_string(in);
-
-index = _strcpy(buffer, s, index);
-
-return (index);
+    return index;
 }
